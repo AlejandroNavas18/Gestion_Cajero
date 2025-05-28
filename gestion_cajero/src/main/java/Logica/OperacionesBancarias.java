431c19da09195026e96a60b0788c3256c53ccddd
AlejandroNavas18 <alejandro.navas.rodriguez.alu@iesjulioverne.es>
2025-05-28 12:52:20 +0200

    cambios.2

--- gestion_cajero/src/main/java/Logica/OperacionesBancarias.java
+++ gestion_cajero/src/main/java/Logica/OperacionesBancarias.java
@@ -3,17 +3,17 @@ package Logica;
 import Modelo.Pojos.Tarjeta;
 
 public class OperacionesBancarias {
-private Tarjeta tarjeta;
+
 
     public void consultarSaldo(Tarjeta tarjeta){
 
     }
 
-    public void retirarEfectivo(int cantidad){
+    public void retirarEfectivo(int cantidad, Tarjeta tarjeta){
 
     }
 
-    public void ingresarEfectivo(int cantidad){
+    public void ingresarEfectivo(int cantidad, Tarjeta tarjeta){
 
     }
 }
